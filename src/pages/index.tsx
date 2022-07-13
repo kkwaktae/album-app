335f6a377dfcbe71c3c078728bfd27f43b8e77d3
taehoon <ender4@naver.com>
2022-07-14 02:38:32 +0900

    setting: CRA, ESlint, Prettier, Stylelint

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header>헤더입니다</header>
+      <main>메인입니다</main>
+      <footer>푸터입니다</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/type/data.d.ts
@@ -0,0 +1,5 @@
+interface ImageInfo {
+  userId: number;
+  id: number;
+  title: string;
+}
