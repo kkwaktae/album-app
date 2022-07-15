67f7c7ef2491341a2d170c30899dc002bf11d7e0
taehoon <ender4@naver.com>
2022-07-15 17:47:18 +0900

    customize: deviding API data to 5

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,2 +1,3 @@
 export { ReactComponent as SearchIcon } from './search-icon.svg';
 export { ReactComponent as AddIcon } from './add-icon.svg';
+export { ReactComponent as EditIcon } from './edit-icon.svg';
