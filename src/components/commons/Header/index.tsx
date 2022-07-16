3eda9c8606cf66fe7ae5957a17a5187b7a872f76
taehoon <ender4@naver.com>
2022-07-16 23:44:04 +0900

    ing: search and filter feature

--- src/components/commons/Header/index.tsx
+++ src/components/commons/Header/index.tsx
@@ -4,14 +4,6 @@ function Header() {
   return (
     <header>
       <h3 className={styles.title}>Album</h3>
-      <div className={styles.userInfoBox}>
-        <p className={styles.userName}>
-          user5<span>님</span>
-        </p>
-        <button type="button" className={styles.loginState}>
-          로그아웃
-        </button>
-      </div>
     </header>
   );
 }
--- src/pages/Main/FeatureSection.tsx
+++ src/pages/Main/FeatureSection.tsx
@@ -1,24 +1,51 @@
-import { CSSProperties, FormEvent, useState } from 'react';
+import {
+  ChangeEventHandler,
+  CSSProperties,
+  FormEventHandler,
+  useState,
+} from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { isFilterAtom, searchAtom } from 'store/atom';
 
-import { AddIcon, SearchIcon } from 'assets/svgs';
+import cx from 'classnames';
+
+import { AddIcon, ResetIcon, SearchIcon } from 'assets/svgs';
 import styles from './styles.module.scss';
 
 function FeatureSection() {
   const [searchBarDisplay, setSearchBarDisplay] = useState(false);
+  const [searchValue, setSearchValue] = useRecoilState(searchAtom);
+  const setIsFilter = useSetRecoilState(isFilterAtom);
 
   const searchBarStyle: CSSProperties = {
     visibility: 'visible',
     opacity: '1',
   };
 
-  const onSubmitSearch = (e: FormEvent<HTMLFormElement>) => {
+  const onSubmitSearch: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
+    setIsFilter(false);
+    if (searchBarDisplay && searchValue.trim().length > 0) {
+      setIsFilter(true);
+    } else {
+      setIsFilter(false);
+    }
     setSearchBarDisplay(true);
   };
 
-  const onClickAddIcon = () => {
-    console.log('aaa');
+  const onChangeSearch: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { value } = e.currentTarget;
+    setSearchValue(value);
   };
+
+  const onClickResetIcon = () => {
+    setSearchValue('');
+  };
+
+  const onClickAddIcon = () => {
+    console.log('add'); // 이미지 추가 기능 구현
+  };
+
   return (
     <section className={styles.featureSection}>
       <form className={styles.featureBox} onSubmit={onSubmitSearch}>
@@ -26,9 +53,19 @@ function FeatureSection() {
           <input
             className={styles.searchbar}
             type="text"
-            placeholder="여기에 검색하세요"
+            placeholder="Search here !"
             style={searchBarDisplay ? searchBarStyle : {}}
+            onChange={onChangeSearch}
+            value={searchValue}
           />
+          <button
+            type="button"
+            className={cx(styles.iconButton, styles.resetButton)}
+            style={searchBarDisplay ? searchBarStyle : {}}
+            onClick={onClickResetIcon}
+          >
+            <ResetIcon className={styles.resetIcon} />
+          </button>
           <button type="submit" className={styles.iconButton}>
             <SearchIcon className={styles.searchIcon} />
           </button>
